305acd4b65735e523e4abb7e4a7a89679d6805c1
Zamri <zamrisuwandi98@gmail.com>
2023-06-05 00:10:21 +0800

    add transaction function

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from databse import database
 
 #the example to call the data
-print(database["history transaction"]["amount"][0])
+#print(database["history transaction"]["amount"][0])
 
-print(type(database))
+#print(type(database))
 
 
 
@@ -17,16 +17,20 @@ for x in database:
 """
 
 id=int(input("key in your id : "))
+id_no=database["id no"]
+index = id_no.index(id)
+current_balance=database["balance"][index]
+
 
 
 
 def key_in_no(x) :
     
-    id_no=database["id no"]
+    
     name=database["name"]
     
     
-    index = id_no.index(x)
+
     
     
     if x in id_no:
@@ -38,10 +42,46 @@ def key_in_no(x) :
         return print(" you id is not exist")
     
     
+    
+def transaction(operation):
+    
+    
+    
+    if operation == "cashout":
+       current_balance_after= current_balance-amount
+    elif operation == "cashin":
+        current_balance_after=current_balance+amount
+
+    
+    
+    return current_balance_after
+        
+        
+    
+    
+    
+    
+
+
+    
 try:    
     key_in_no(id)
+    
 
 except :
     print("you id is not exist or your key in invalid id")
-    
+
+
+print(current_balance)
+operation=input("cashout,cashin or check balance ")
+amount =int(input("amount : "))
+
+current_balance_after=transaction(operation)
+print(current_balance_after)
+
+
+
+
+
+
 
